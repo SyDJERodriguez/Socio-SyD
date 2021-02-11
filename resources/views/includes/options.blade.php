4c52ca2391a5eb746d0b04702f1ba47ba8247bcf
eduarlalo <laguirre@quaxar.com>
2021-02-11 16:54:42 -0600

    include options added (buttons & arrow)

---
+++ resources/views/includes/options.blade.php
@@ -0,0 +1,54 @@
+<div class="container-fluid mt-3 pr-5 pl-5">
+    <div class="row inside_nav someimpor">
+        <div class="col-lg-2 py-2 border-right border-primary offset-lg-1">
+            <a href="#" class="btn btn-lg btn-block boton active" style="background-color: #009CE0;">Beneficios obtenidos</a>
+        </div>
+        <div class="col-lg-2 py-2 border-right border-primary">
+            <a href="#" class="btn btn-lg btn-block boton">agrEgar dependientes</a>
+        </div>
+        <div class="col-lg-2 py-2 border-right border-primary">
+            <a href="#" class="btn btn-lg btn-block boton">estado de cuenta</a>
+        </div>
+        <div class="col-lg-2 py-2 border-right border-primary">
+            <a href="#" class="btn btn-lg btn-block boton">mis documentos</a>
+        </div>
+        <div class="col-lg-2 py-2">
+            <a href="#" class="btn btn-lg btn-block boton">referir amigos</a>
+        </div>
+    </div>
+</div>
+<div class="container-fluid mt-1 font-weight-bold" style="font-size: 14px;">
+    <div class="row">
+        <div class="col-lg-3 text-right pl-5">
+            <span>REGISTRO DE BENEFICIARIOS
+            </span>
+        </div>
+        <div class="col-lg-3 text-center pl-5"> <span>ESTUDIO SOCIOECONÓMICO</span></div>
+        <div class="col-lg-3 text-center pr-5"> <span>SUBIR DOCUMENTOS</span></div>
+        <div class="col-lg-3 text-left pl-5"> <span>FIRMA ELECTRÓNICA</span></div>
+    </div>
+</div>
+</div>
+<div class="container-fluid">
+    <div class="d-flex justify-content-center mt-1">
+        <div class="arrow-steps clearfix">
+            <div class="row">
+                <div class="col-lg-3 py-2 px-0">
+                    <div class="step current"> <span class="float-right"> 25%</span> </div>
+                </div>
+                <div class="col-lg-3 py-2 px-0">
+                    <div class="step"> <span class="float-right">50%</span> </div>
+                </div>
+                <div class="col-lg-3 py-2 px-0">
+                    <div class="step" style="background-color: #009CE0;"> <span class="float-right"> 75%</span> </div>
+                </div>
+                <div class="col-lg-3 py-2 px-0">
+                    <div class="step" style="background-color: #009CE0;"> <span class="float-right">100%</span> </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<h6 class="text-center">PROGRESO DE REGISTRO</h6>
--- resources/views/pages/beneficiario.blade.php
+++ resources/views/pages/beneficiario.blade.php
@@ -1,62 +1,12 @@
 @extends('layouts.application')
 @section('content')
-<!-- aqui-->
-<div class="container-fluid mt-3 pr-5 pl-5">
-    <div class="row inside_nav someimpor">
-        <div class="col-lg-2 py-2 border-right border-primary offset-lg-1">
-            <a href="#" class="btn btn-lg btn-block boton active" style="background-color: #009CE0;">Beneficios obtenidos</a>
-        </div>
-        <div class="col-lg-2 py-2 border-right border-primary">
-            <a href="#" class="btn btn-lg btn-block boton">agrEgar dependientes</a>
-        </div>
-        <div class="col-lg-2 py-2 border-right border-primary">
-            <a href="#" class="btn btn-lg btn-block boton">estado de cuenta</a>
-        </div>
-        <div class="col-lg-2 py-2 border-right border-primary">
-            <a href="#" class="btn btn-lg btn-block boton">mis documentos</a>
-        </div>
-        <div class="col-lg-2 py-2">
-            <a href="#" class="btn btn-lg btn-block boton">referir amigos</a>
-        </div>
-    </div>
-</div>
-<div class="container-fluid mt-1 font-weight-bold" style="font-size: 14px;">
-    <div class="row">
-        <div class="col-lg-3 text-right pl-5">
-            <span>REGISTRO DE BENEFICIARIOS
-            </span>
-        </div>
-        <div class="col-lg-3 text-center pl-5"> <span>ESTUDIO SOCIOECONÓMICO</span></div>
-        <div class="col-lg-3 text-center pr-5"> <span>SUBIR DOCUMENTOS</span></div>
-        <div class="col-lg-3 text-left pl-5"> <span>FIRMA ELECTRÓNICA</span></div>
-    </div>
-</div>
-</div>
-<div class="container-fluid">
-    <div class="d-flex justify-content-center mt-1">
-        <div class="arrow-steps clearfix">
-            <div class="row">
-                <div class="col-lg-3 py-2 px-0">
-                    <div class="step current"> <span class="float-right"> 25%</span> </div>
-                </div>
-                <div class="col-lg-3 py-2 px-0">
-                    <div class="step"> <span class="float-right">50%</span> </div>
-                </div>
-                <div class="col-lg-3 py-2 px-0">
-                    <div class="step" style="background-color: #009CE0;"> <span class="float-right"> 75%</span> </div>
-                </div>
-                <div class="col-lg-3 py-2 px-0">
-                    <div class="step" style="background-color: #009CE0;"> <span class="float-right">100%</span> </div>
-                </div>
-            </div>
 
-        </div>
-    </div>
-</div>
+<!--BUTTONS AND PROGRESS ARROW-->
+@extends('includes.options')
+
 <div class="barra">
     <div class="row">
         <div class="col-lg-12">
-            <h6 class="text-center">PROGRESO DE REGISTRO</h6>
             <hr style="background-color: #009CE0; height:2px; margin-left: 10px;margin-right: 10px ">
         </div>
         <div class="col-lg-3" style="margin-left: 10px">
