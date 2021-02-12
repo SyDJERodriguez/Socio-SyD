e8724056dd49d26fd2fc22aa266d3cb34862b283
eduarlalo <laguirre@quaxar.com>
2021-02-12 17:03:44 -0600

    popUp Notifications added

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -10,24 +10,30 @@
             <form id="login-form">
                 <div class="form-row align-items-center">
                     <div class="col-sm-5 my-1">
-                        <input type="text" class="form-control border-input" id="inlineFormInputName" placeholder="CORREO">
+                        <input type="text" class="form-control border-input" id="inlineFormInputName"
+                            placeholder="CORREO">
                         <div class="col-sm-12" style="display: flex; justify-content: center; align-items: flex-end;">
                             <p class="primary-color pr-3" style="margin-bottom: 0"><b>¿No tienes una cuenta?</b></p>
-                            <a href="#" class="btn btn p-0" style="background-color: #143153;color:white;font-size: 12px; width: 90px;" data-toggle="modal" data-target="#modalClientType">¡REGISTRATE!</a>
+                            <a href="#" class="btn btn p-0"
+                                style="background-color: #143153;color:white;font-size: 12px; width: 90px;"
+                                data-toggle="modal" data-target="#modalClientType">¡REGISTRATE!</a>
                         </div>
                     </div>
                     <div class="col-sm-5 my-1">
-                        <input type="text" class="form-control border-input" id="inlineFormInputGroupUsername" placeholder="CONTRASEÑA">
+                        <input type="text" class="form-control border-input" id="inlineFormInputGroupUsername"
+                            placeholder="CONTRASEÑA">
                         <div class="col-sm-12" style="display: flex; justify-content: center; align-items: flex-end;">
-                            <a href="#" class="primary-color" data-toggle="modal" data-target="#modal4"><b>¿Olvidaste tu contraseña?</b></a>
+                            <a href="#" class="primary-color" data-toggle="modal" data-target="#modal4"><b>¿Olvidaste tu
+                                    contraseña?</b></a>
                         </div>
                     </div>
 
                     <div class="col-sm-2 my-1">
-                        <button type="submit" class="btn btn px-2" style="background-color: #143153;color:white; width: 200px">INICIAR SESIÓN</button>
+                        <button type="submit" class="btn btn px-2"
+                            style="background-color: #143153;color:white; width: 200px">INICIAR SESIÓN</button>
                         <a href="#" class="disabl">Iniciar sesión</a>
                     </div>
-                    
+
                     <!-- <div class="col-sm-2 my-1">
                         <button type="submit" class="btn btn px-2"
                         style="background-color: #143153;color:white; width: 200px"
@@ -39,50 +45,84 @@
         </div>
         <div class="col-lg-2 notification" style="padding-left: 160px;padding-top: 25px;">
             <div class="btn-group dropleft">
-                <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <span class="badge badge-danger mr-1 rounded-circle" style="font-size:10px;">1</span><i class="far fa-bell" style="color: white; font-size: 28px;"> </i>
+                <button type="button" class="btn dropdown-toggle" data-toggle="modal" aria-haspopup="true"
+                    aria-expanded="false" data-target="#modalNotifications">
+                    <span class="badge badge-danger mr-1 rounded-circle" style="font-size:10px;">1</span>
+                    <i class="far fa-bell" style="color: white; font-size: 28px;"> </i>
                 </button>
-                <div class="dropdown-menu">
-                    <a class="dropdown-item" href="#">Action</a>
-                    <a class="dropdown-item" href="#">Another action</a>
-                    <a class="dropdown-item" href="#">Something else here</a>
-                    <div class="dropdown-divider"></div>
-                    <a class="dropdown-item" href="#">Separated link</a>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal NOTIFICATIONS-->
+    <div class="modal fade" id="modalNotifications" tabindex="-1" role="dialog" aria-labelledby="modalNotifications"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="toast-header text-white blue-dark p-0" >
+                    <strong class="mr-auto ml-4">Notificaciones</strong>
+                    <button type="button" class="ml-2  close text-white p-2" 
+                    data-dismiss="modal" aria-label="Close" style="background-color:#00A5E6">
+                      <span aria-hidden="true">&times;</span>
+                    </button>
+                  </div>
+                
+                <div class="modal-body">
+                    <ul class="list-group list-group-flush">
+                        <li class="list-group-item"><span class="float-left" style="font-size: 14px;">¡Ya tienes derecho a tu
+                                seguro<bR> de accidentes personales</span><span class="float-right"><i style="color: #00A5E6;"
+                                    class="fas fa-circle"></i></span></li>
+                        <li class="list-group-item"><span class="float-left" style="font-size: 14px;">Lorem ipsum dolor sit
+                                amet, consectetuer<br> adipiscing elit, sed diam </span><span class="float-right"><i
+                                    style="color: #00A5E6;" class="far fa-check"></i></span></li>
+                        <li class="list-group-item"><span class="float-left" style="font-size: 14px;">Lorem ipsum dolor sit
+                                amet, consectetuer<br> adipiscing elit, sed diam </span><span class="float-right"><i
+                                    style="color: #00A5E6;" class="far fa-check"></i></span></li>
+                        <li class="list-group-item"><span class="float-left" style="font-size: 14px;">Lorem ipsum dolor sit
+                                amet, consectetuer<br> adipiscing elit, sed diam </span><span class="float-right"><i
+                                    style="color: #00A5E6;" class="far fa-check"></i></span></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 
     <!-- Modal LOGOUT-->
-    <div class="modal fade" id="modalLogOut" tabindex="-1" role="dialog" aria-labelledby="modalLogOut" aria-hidden="true">
+    <div class="modal fade" id="modalLogOut" tabindex="-1" role="dialog" aria-labelledby="modalLogOut"
+        aria-hidden="true">
         <div class="modal-dialog modal-lg" role="document">
             <div class="modal-content border-0 rounded-0" style="background:transparent">
                 <div class="modal-header" style="height: 35px;">
-                  <button type="button" class="close" style="margin: 0rem 0rem -1rem auto;padding: 0.1rem 1rem 0.5rem;background-color: #00A5E6;"  data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true" class="text-white">&times;</span>
-                  </button>
+                    <button type="button" class="close"
+                        style="margin: 0rem 0rem -1rem auto;padding: 0.1rem 1rem 0.5rem;background-color: #00A5E6;"
+                        data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true" class="text-white">&times;</span>
+                    </button>
                 </div>
                 <div class="modal-body blue-dark">
                     <div>
-                        <div>  
-                            <h5 class="text-white"><b>¿ESTÁS SEGURO QUE DESEAS SALIR?</b></h5>  
+                        <div>
+                            <h5 class="text-white"><b>¿ESTÁS SEGURO QUE DESEAS SALIR?</b></h5>
                         </div>
                     </div>
-                  <div class="row">
-                      <div class="col-lg-9 py-2 text-center">
-                        <img src="{{asset('img/logo.png')}}" alt="logo" width="50%"></div>
-                      <div class="col-lg-3 py2 text-center">
-                          <input type="button" class="text-white btn btn bg-primary btn-sm my-2" value="si" style="padding-left: 40px;padding-right: 40px;"><br>
-                          <input type="button" class="btn btn-light btn-sm" value="No" style="padding-left: 35px;padding-right: 35px;background-color: white;color: #00A5E6;">
-                      </div>
-                  </div>
+                    <div class="row">
+                        <div class="col-lg-9 py-2 text-center">
+                            <img src="{{asset('img/logo.png')}}" alt="logo" width="50%"></div>
+                        <div class="col-lg-3 py2 text-center">
+                            <input type="button" class="text-white btn btn bg-primary btn-sm my-2" value="si"
+                                style="padding-left: 40px;padding-right: 40px;"><br>
+                            <input type="button" class="btn btn-light btn-sm" value="No"
+                                style="padding-left: 35px;padding-right: 35px;background-color: white;color: #00A5E6;">
+                        </div>
+                    </div>
                 </div>
-              </div>
+            </div>
         </div>
     </div>
 
     <!-- Modal CLIENT TYPE-->
-    <div class="modal fade" id="modalClientType" tabindex="-1" role="dialog" aria-labelledby="modalClientType" aria-hidden="true">
+    <div class="modal fade" id="modalClientType" tabindex="-1" role="dialog" aria-labelledby="modalClientType"
+        aria-hidden="true">
         <div class="modal-dialog modal-sm" role="document">
             <div class="modal-content">
                 <div class="container ">
@@ -119,7 +159,8 @@
     <a class="navbar-brand" href="#">
         <img class="logo-menu" src="{{asset('img/logo.png')}}" width="120px" alt="">
     </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <i class="fas fa-bars text-white"></i>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
