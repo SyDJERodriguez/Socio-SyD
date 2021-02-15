38aac79105086d324866446ae856ddcfd8cd40d1
Frank Reyes <fmartinez@quaxar.com>
2021-02-15 08:08:36 -0600

    Fix some issues

--- resources/views/pages/Account/documents.blade.php
+++ resources/views/pages/Account/documents.blade.php
@@ -7,34 +7,34 @@
         <div class="col-lg-12">
             <hr>
         </div>
-        <div class="col-lg-3">
+        <div class="col-lg-2">
             <h6> Hola EDUARDO Martínez Pozos</h6>
             <h6> No. de Cliente <span style="color: #009CE0;">{{substr(Auth::user()->client_number, 2)}}</span></h6>
             <br/>
             <br/>
             <div>
-              <img src="{{asset('img/mandoc.png')}}"  style="width: 90%;border-radius: 10px;" alt="">
+              <img src="{{asset('img/mandoc.png')}}"  style="width: 90%;border-radius: 10px; margin-bottom: 50px" alt="">
             </div>
         </div>
-        <div class="col-lg-9 py-5">
+        <div class="col-lg-10 py-5">
             <form>
                 <div class="form-row text-center" style="border: 1px solid rgba(128, 128, 128, 0.664);padding: 40px;border-radius: 10px;">
                     <div class="col-lg-12" style="color: #143153;">  <h5 class="text-center"> <strong> DA CLICK EN CADA SECCIÓN PARA VER O EDITAR TUS DOCUMENTOS</strong></h5>
                     </div>
-                  <div class="col-lg-6 py-3 text-center">
+                  <!--<div class="col-lg-6 py-3 text-center">
                       <img src="{{asset('img/Asset8.png')}}" class="py-2"><br>
                     <input type="button" class="btn btn py-2  text-white " style="background-color: #143153;" value="estudio socioeconómico">
-                  </div>
-                  <div class="col-lg-6 py-3 text-center">
+                  </div>-->
+                  <div class="col-lg-12 py-3 text-center">
                     <img src="{{asset('img/Asset9.png')}}" class="py-2"><br>
                     <input type="button" class="btn btn py-2  text-white " style="background-color: #143153;" value="Certificado de la póliza">                  </div>
-                  <div class="col-lg-6 py-3 text-center">
+                  <!--<div class="col-lg-6 py-3 text-center">
                     <img src="{{asset('img/Asset10.png')}}" class="py-2"><br>
                     <input type="button" class="btn btn py-2  text-white " style="background-color: #143153;padding-left: 80px;padding-right: 80px;" value="INE">  </div>
                   <div class="col-lg-6 py-3 text-center">
                     <img src="{{asset('img/Asset11.png')}}" class="py-2"><br>
                     <input type="button" class="btn btn py-2  text-white " style="background-color: #143153;" value="comprobante de domicilio">
-                  </div>
+                  </div>-->
                 
              
                 </div>
--- routes/web.php
+++ routes/web.php
@@ -28,6 +28,10 @@ Route::prefix('customer')->name('customer.')->group(function(){
     Route::group(['middleware' => ['auth:customer']], function() {
         Route::get('/account/', 'CustomerController@account_status')->name('myAccount');
         Route::get('/documents/', 'CustomerController@my_documents')->name('myDocuments');
+
+        //Benefits
+        Route::get('/benefits/beneficiary/', 'CustomerController@register_beneficiary')->name('register.beneficiary');
+        Route::get('benefits/signature/', 'CustomerController@benefits_signature')->name('benefits.signature');
         Route::post('/logout', 'CustomerController@logout')->name('logout');
     });
 });
