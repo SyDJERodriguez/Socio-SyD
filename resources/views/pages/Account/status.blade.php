700da5863b7e69de41220ecd85606a6c14c25348
eduarlalo <laguirre@quaxar.com>
2021-02-16 14:22:09 -0600

    dataTable plugin added with print/excel button

--- resources/views/pages/Account/status.blade.php
+++ resources/views/pages/Account/status.blade.php
@@ -2,39 +2,28 @@
 @section('content')
 <!--BUTTONS AND PROGRESS ARROW-->
 @include('includes.options', ['active' => 3])
-<div class="container-fluid pr-5 pl-5" style="padding-left: 3rem !important; padding-right: 3rem !important;">
+@section('css')
+   
+@endsection
+<div style="padding-left: 3rem !important; padding-right: 3rem !important;">
    <hr>
-   <div class="row">
-      <div class="col-lg-4 pl-5" style="padding-left: 15px !important;">
+   <div>
+      <div style="padding-left: 15px !important;">
          <h4>Hola {{$data->name.' '.$data->last_name.' '.$data->second_last_name}}<br>
             No. de Cliente <span style="color:#009ce0">{{substr(Auth::user()->client_number, 2)}}</span>
          </h4>
          <hr>
       </div>
-      <div class="col-lg-8">
+      <div>
       </div>
-      <div class="col-lg-12 pl-5" style="padding-left: 15px !important;">
+      <div style="padding-left: 15px !important;">
          <h4>Detalle de la cuenta</h4>
-          <!--<div class="row">
-             <div class="col-lg-3"><span>Saldo actual:</span></div>
-             <div class="col-lg-3"><span>$2,500.00</span></div>
-             <div class="col-lg-6"><span>Fecha de último movimiento:	</span><span>16 / AGO / 2020</span></div>
-             <div class="col-lg-3"><span>Compras:</span></div>
-             <div class="col-lg-3"><span>$1,800.00</span></div>
-             <div class="col-lg-6"></div>
-             <div class="col-lg-6">
-                <h4 style="color: #143153;"><strong>Movimientos</strong></h4>
-             </div>
-             <div class="col-lg-6"><span class="float-right"><strong style="color: #143153;"><i class="fas fa-caret-right"></i>exportar</strong></span>
-                <span class="float-right px-4" style="color: #143153;"><strong><i class="fas fa-caret-right"></i>Imprimir</strong></span>
-             </div>
-          </div>-->
-       </div>
+         </div>
    </div>
-   <div class="container-fluid py-4" style="overflow-y: scroll">
-      <div class="row" >
-         <div class="col-lg-12 mx-auto " >
-            <table class="table">
+   <div>
+      <div style="margin-left: 15px;">
+         <div>
+            <table class="table table-striped table-bordered" id="tableTrans" style="width:100%">
                <thead>
                   <tr>
                      <!-- <th scope="col">Pieza</th> -->
@@ -50,21 +39,16 @@
                <tbody>
                   @foreach ($tr as $trans)
                      <tr>
-                        <th>{{ $trans->material_type }}</th>
-                        <td>{{ $trans->sale_office }}</td>
+                        <th> {{ $trans->material_type }}</th>
+                        <td> {{ $trans->sale_office }}</td>
                         <td>${{ $trans->amount }}</td>
-                        <td>{{ $trans->payment_method }}</td>
-                        <td>{{ $trans->quantity }}</td>
-                        <td>{{ $trans->transaction_date }}</td>
+                        <td> {{ $trans->payment_method }}</td>
+                        <td> {{ $trans->quantity }}</td>
+                        <td> {{ $trans->transaction_date }}</td>
                      </tr>
                   @endforeach
                </tbody>
             </table>
-         </div>
-      </div>
-      <div class="row text-center" style="margin-top: -35px">
-         <div class="col-12">
-            <i class="fa fa-caret-down fa-3x"></i>
          </div>
       </div>
       <br>
@@ -77,7 +61,7 @@
    <br>
    <div class="row">
       <div class="col-12 ">
-         <span class="float-left pl-5">
+         <span class="float-left pl-2">
             <h4 style="color: #143153;"><strong>ACLARACIONES</strong></h4>
             <p>•	Para aclaraciones llámanos <br>
                al 800 SYD (793) 1010
@@ -88,5 +72,15 @@
    </div>
    <br><br><br><br>
 </div>
+
+<script>
+   $('#tableTrans').DataTable({
+      dom: 'Bfrtip',
+      searching:false,
+      buttons: [
+            'excel', 'print'
+        ]
+   });
+</script>
 
 @stop
