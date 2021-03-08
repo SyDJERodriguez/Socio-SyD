39859574243ef97b9af4497496ff7b762f82f972
eduarlalo <laguirre@quaxar.com>
2021-03-08 17:38:01 -0600

    uppercase modal added

--- resources/views/includes/Account/deleteButton.blade.php
+++ resources/views/includes/Account/deleteButton.blade.php
@@ -63,7 +63,7 @@
                 <div class="col-lg-12 py-2 text-center">
                     <img src="{{asset('img/logo.png')}}" alt="logo" width="50%"></div>
                 <div>
-                    <h5 class="text-white"><b>AL CLICK EN ACEPTAR, CONFIRMAS QUE:<br>
+                    <h5 class="text-white"><b>AL HACER CLICK EN ACEPTAR, CONFIRMAS QUE:<br>
                     ESTAS CONSCIENTE DE QUE PERDERAS TUS BENEFICIOS DE SEGURO Y ASISTENCIAS.</b></h5>
                 </div>
                 <div class="row">
