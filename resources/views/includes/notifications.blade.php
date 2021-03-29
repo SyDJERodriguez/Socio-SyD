764664d5fd74f122560843d9887d4dddb4692343
Frank Reyes <fmartinez@quaxar.com>
2021-03-29 16:33:00 -0600

    Add aceptar button for notifications in mechanic user

--- resources/views/includes/notifications.blade.php
+++ resources/views/includes/notifications.blade.php
@@ -14,46 +14,46 @@
                         <ul class="list-group list-group-flush">
                             @if ($total)
                                     @if ($total < 2500.01)
-                                    <li class="list-group-item">
-                                        <span class="float-right">
-                                            <i style="color: #00A5E6;" class="fas fa-circle" hidden></i></span>
-                                            <p class="float-left" style="font-size: 14px;width:95%">
-                                            Aun no alcanzas el nivel minimo de lealtead.</p>
-                                    </li>
+                                        <li class="list-group-item">
+                                            <span class="float-right">
+                                                <i style="color: #00A5E6;" class="fas fa-circle" hidden></i></span>
+                                                <p class="float-left" style="font-size: 14px;width:95%">
+                                                Aun no alcanzas el nivel minimo de lealtead.</p>
+                                        </li>
                                     @else
-                                    <li class="list-group-item">
-                                        <span class="float-right">
-                                            <i style="color: #00A5E6;" class="fas fa-circle"></i></span>
-                                            <p class="float-left" style="font-size: 14px;width:95%">
-                                            Tu nivel de lealtead actual es: 
-                                                @if ($total > 2500 && $total <= 4500)
-                                                    Bronce
-                                                @elseif($total > 4500 && $total <= 7000)
-                                                    Plata
-                                                @elseif($total > 7000)
-                                                    Oro
-                                                @endif
-                                            </p>
-                                    </li>
-                                    <li class="list-group-item">
-                                        <span class="float-right">
-                                            <i style="color: #00A5E6;" class="far fa-check"></i></span>
-                                            <p class="float-left" style="font-size: 14px;width:95%">
-                                            ¡Ya tienes derecho a tu seguro de accidentes personales!</p>
-                                    </li>
+                                        <li class="list-group-item">
+                                            <span class="float-right">
+                                                <i style="color: #00A5E6;" class="fas fa-circle"></i></span>
+                                                <p class="float-left" style="font-size: 14px;width:95%">
+                                                Tu nivel de lealtead actual es:
+                                                    @if ($total > 2500 && $total <= 4500)
+                                                        Bronce
+                                                    @elseif($total > 4500 && $total <= 7000)
+                                                        Plata
+                                                    @elseif($total > 7000)
+                                                        Oro
+                                                    @endif
+                                                </p>
+                                        </li>
+                                        <li class="list-group-item">
+                                            <span class="float-right">
+                                                <i style="color: #00A5E6;" class="far fa-check"></i></span>
+                                                <p class="float-left" style="font-size: 14px;width:95%">
+                                                ¡Ya tienes derecho a tu seguro de accidentes personales!</p>
+                                        </li>
                                     @if ($total > 4500)
-                                    <li class="list-group-item">
-                                        <span class="float-right">
-                                            <i style="color: #00A5E6;" class="far fa-check"></i></span>
-                                            <p class="float-left" style="font-size: 14px;width:95%">
-                                            Ya eres acreedor al nivel de asistencia 
-                                               @if ($total > 4500 && $total <= 7000)
-                                                    Plata
-                                               @elseif($total > 7000)
-                                                    Oro
-                                               @endif  
-                                            </p>
-                                    </li>
+                                        <li class="list-group-item">
+                                            <span class="float-right">
+                                                <i style="color: #00A5E6;" class="far fa-check"></i></span>
+                                                <p class="float-left" style="font-size: 14px;width:95%">
+                                                Ya eres acreedor al nivel de asistencia
+                                                   @if ($total > 4500 && $total <= 7000)
+                                                        Plata
+                                                   @elseif($total > 7000)
+                                                        Oro
+                                                   @endif
+                                                </p>
+                                        </li>
                                     @endif
                                     <br>
                                     <a class="btn btn-sm btn-block" href="{{route('customer.dismissNotification', Auth::user()->client_number)}}">
@@ -114,7 +114,10 @@
                                         </p>
                                 </li>
                                 @endif
-                            
+                                <br>
+                                <a class="btn btn-sm btn-block" href="{{route('customer.dismissNotification', Auth::user()->client_number)}}">
+                                    Aceptar
+                                </a>
                             @endif
                         </ul>
 
