5147e8ef48e58b28dad4e1a90c8e65fe38d56a99
eduardo-af <laguirre@quaxar.com>
2021-04-26 18:21:32 -0500

    ortografia fixes in home page

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,11 +4,16 @@
         <div class="container-fluid">
             <div class="row m-0">
                 <div class="col-lg-6 blue-dark text-center px-auto py-5">
-                    <h1 class="nav-link"> <STRONG>SOCIO SyD®</STRONG></h1>
-                    <h3 class="nav-link">ES EL PROGRAMA<br>
-                        EN EL QUE TODOS LOS MECÁNICOS<BR>RECIBEN BENEFICIOS,<br>
-                        POR HACER LAS COMPRAS DE SIEMPRE.
-                    </h3>
+                    <br>
+                    <p class="nav-link" style="font-size: 30px;line-height:90%"> 
+                        <STRONG>SOCIO SyD®</STRONG>
+                        <br>
+                        <span style="font-size: 23px"> ES EL PROGRAMA<br>
+                        EN EL QUE TODOS LOS MECÁNICOS<BR>
+                        RECIBEN BENEFICIOS,<br>
+                        POR HACER LAS COMPRAS DE SIEMPRE
+                        </span>
+                    </p>
                 </div>
                 <div class="col-lg-6 p-0">
                     <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
@@ -45,28 +50,50 @@
         <div class="p-5">
             <div class="row">
                 <div class="col-lg-3 py-5"><div class="bg-primary bar" style="height: 2px;"></div></div>
-                <div class="col-lg-6 py-3"> <h1 class="primary-color text-center"><strong>CÓMO FUNCIONA SOCIO SyD®</strong></h1></div>
+                <div class="col-lg-6 py-3"> 
+                    <h2 class="primary-color text-center">
+                        <strong>CÓMO FUNCIONA SOCIO SyD®</strong>
+                    </h2>
+                </div>
                 <div class="col-lg-3 py-5"><div class="bg-primary bar" style="height: 2px;"></div></div>
             </div>
             <div class="row primary-color">
                 <div class="col-lg-4 mx-auto text-center img1">
                     <img class="mx-auto py-2" src="{{asset('img/icon_pc.png')}}" alt="">
-                    <h2 class="text-center pt-3"><strong>REGÍSTRATE</strong></h2>
-                    <h5 style="font-weight: 400;">Hazlo desde<br>
-                        cualquier dispositivo.</h5>
+                    <p class="text-center" style="font-size: 18px;line-height:90%">
+                        <br><br>
+                        <strong>REGÍSTRATE</strong>
+                        <br><br>
+                    <span style="font-size:15px;">
+                        Hazlo desde<br>
+                        cualquier dispositivo
+                        </span>
+                    </p>
                 </div>
                 <div class="col-lg-4 mx-auto text-center img2">
-                    <img class="mx-auto py-2" src="{{asset('img/shopping.png')}}" alt="">
-                    <h2 class="text-center pt-3"><strong>COMPRA</strong></h2>
-                    <h5 style="font-weight: 400;">Haz una compra mínima<br>
-                        de <b>$200</b> pesos al mes <br> en cualquier sucursal <br> DAR Refaccionaria.</h5>
+                    <img class="mx-auto py-2" src="{{asset('img/shopping.png')}}" alt=""
+                    height="90px">
+                    <p class="text-center" style="font-size: 18px;line-height:90%">
+                        <br>
+                        <strong>COMPRA</strong>
+                        <br><br>
+                    <span style="font-size:15px;">
+                        Haz una compra mínima<br>
+                        de <b>$200</b> pesos al mes <br> en cualquier sucursal <br> 
+                        DAR Refaccionarias
+                    </span>
+                    </p>
                 </div>
                 <div class="col-lg-4 mx-auto text-center img3">
                     <img class="mx-auto py-2 pb-3" src="{{asset('img/check.png')}}" alt="">
-                    <h2 class="text-center pt-3"><strong>OBTÉN BENEFICIOS</strong></h2>
-                    <h5 style="font-weight: 400;">
+                    <p class="text-center"  style="font-size: 18px;line-height:90%">
+                        <strong>OBTÉN BENEFICIOS</strong>
+                        <br><br><br>
+                    <span  style="font-size:15px;">
                         Obtén tu seguro de accidentes<br>
-                        y más beneficios.</h5>
+                        y más beneficios
+                    </span>
+                    </p>
                 </div>
             </div>
         </div>
@@ -127,7 +154,7 @@
                                     • Seguro contra accidentes<br>
                                     • Asistencia las 24 horas<br>
                                 </strong>
-                                Te brindamos la seguridad que nadie te da.
+                                Te brindamos la seguridad que nadie te da
                             </p>
                         </div>
                         <div class="my-4 mb-5">
@@ -151,15 +178,15 @@
                     <img src="{{asset('img/segor.png')}}" class="pb-4" alt="">
                     <h5 class="text-white mt-2"><strong>SEGURO DE ACCIDENTES<br>
                             PERSONALES</strong></h5>
-                    <p>Hasta $50,000 pesos<br>por muerte accidental.</p>
+                    <p>Hasta $50,000 pesos<br>por muerte accidental</p>
                 </div>
                 <div class="col-lg-6 text-center text-white" style="cursor: pointer;" data-toggle="modal" data-target="#modalAsistencias">
                     <img src="{{asset('img/servicios.png')}}" class="pb-4" alt="">
                     <h5 class="text-white"><strong>SERVICIOS<br>
                         DE ASISTENCIA</strong></h5>
-                    <p>Asistencia Médica 24 horas<br>
-                        /Servicio de Grúa/Ambulancia<br>Terrestre/Servicio Funerario<br>
-                        por hasta $10,000.</p>
+                    <p>Asistencia Médica 24 horas/<br>
+                        Servicio de Grúa/Ambulancia<br>Terrestre/Servicio Funerario<br>
+                        por hasta $10,000</p>
                 </div>
             </div>
         </div>
@@ -290,12 +317,13 @@
                                     </div>
                                 </div>
 
-                                <div class="col-2 mapIcon">
+                                <div class="col-2 py-2 mapIcon">
                                     <svg aria-hidden="true" width="30" focusable="false" data-prefix="fab" data-icon="whatsapp" class="svg-inline--fa fa-whatsapp fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="#00A1E3" d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7.9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"></path></svg>
                                 </div>
                                 <div class="col-9 py-1">
                                     <div class="our-branches__content-address-location-result-item-body item_whats sucursal text-wrap" style="padding-left: 15px">
-                                        <p> 55-1016-8974<br></p>
+                                        <p> 55-1016-8974</p>
+                                        <br>
                                     </div>
                                 </div>
 
