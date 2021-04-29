602386798d90375d6a4b24dcbb42f44e6244fcb1
eduardo-af <laguirre@quaxar.com>
2021-04-28 20:07:50 -0500

    sideBarBenefits level TODO

--- resources/views/includes/Account/sidebarBenefits.blade.php
+++ resources/views/includes/Account/sidebarBenefits.blade.php
@@ -10,6 +10,13 @@
             @else
                 Cuenta: Individual
             @endif
+            {{-- <br>
+            Nivel Actual: 
+                @if ($level === 'oro' || $level === 3)
+                Oro
+                @elseif($level === 'plata' || $level === 2)
+                Plata
+                @endif --}}
         </h6>
         <a href="#" class="btn btn" data-toggle="modal" data-target="#survey" style="background-color: #00A1E3;color: #FFF;">Nos interesa tu opinión</a>
         <hr>
