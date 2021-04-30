f38d244a60d982b6a5c7142a7547e7179a8db593
eduardo-af <laguirre@quaxar.com>
2021-04-29 19:18:16 -0500

    iconTel and text fixed

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -329,14 +329,13 @@
                                 </div>
 
                                 <div class="col-2 mapIcon" 
-                                style="height: 66px;
-                                display: flex;
+                                style="display: flex;
                                 justify-content: center;
                                 align-items: center;">
                                     <svg aria-hidden="true" width="30" focusable="false" data-prefix="fas" data-icon="phone-alt" class="svg-inline--fa fa-phone-alt fa-w-16 telIcon" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#00A1E3" d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z"></path></svg>
                                 </div>
                                 <div class="col-9" 
-                                style="height: 66px;
+                                style="height: 100%;
                                 display: flex;
                                 align-items: center;">
                                     <div class="our-branches__content-address-location-result-item-body item_tel sucursal text-wrap" style="padding-left: 15px;">
