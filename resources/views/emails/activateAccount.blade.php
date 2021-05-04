b35b203f9e49345810ccd3a58eeee95e6f3973fa
eduardo-af <laguirre@quaxar.com>
2021-05-04 12:20:34 -0500

    header error message fixed; button signUp fixed; email recovery text fixed

--- resources/views/emails/activateAccount.blade.php
+++ resources/views/emails/activateAccount.blade.php
@@ -1,5 +1,5 @@
 <div>
     <p>Hola {{$data['name'].' '.$data['last_name']}}</p>
 
-    <p>Has solicitado restablecer tu cuenta en la plataforma SYD, has clic este <a href="{{url('account/activate/'.$data['client_number'])}}">enlace</a>  para continuar.</p>
+    <p>Has solicitado reestablecer tu cuenta en la plataforma SYD, has clic este <a href="{{url('account/activate/'.$data['client_number'])}}">enlace</a> para continuar.</p>
 </div>
--- resources/views/emails/restorePassword.blade.php
+++ resources/views/emails/restorePassword.blade.php
@@ -1,5 +1,5 @@
 <div>
     <p>Hola {{$data['name'].' '.$data['last_name']}}</p>
 
-    <p>Has solicitado restablecer tu contraseña de acceso a la plataforma SYD, has clic este <a href="{{url('password/edit/'.$data['client_number'])}}">enlace</a>  para continuar.</p>
+    <p>Has solicitado reestablecer tu contraseña de acceso a la plataforma SYD, has clic este <a href="{{url('password/edit/'.$data['client_number'])}}">enlace</a>  para continuar.</p>
 </div>
