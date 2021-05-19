9b8d6e36ce0cbe54bbd5e836d0b979ab2a345636
eduardo-af <laguirre@quaxar.com>
2021-05-18 21:21:40 -0500

    confirmar contraseña input fixed layout in form

--- resources/views/includes/formularioDueño.blade.php
+++ resources/views/includes/formularioDueño.blade.php
@@ -82,15 +82,15 @@
                 
                 <div class="row ">
                     <div class="col-lg-6 py-2" style="display: flex">
-                        <input type="text" class="form-control btnBorder" placeholder="R.F.C" id="rfc" name="rfc" required>
-                        <p style="color: red; margin: 0;">*</p>
-                    </div>
-                    <div class="col-lg-6 py-2" style="display: flex">
                         <input autocomplete="new-password" type="password" class="form-control btnBorder" placeholder="CONTRASEÑA" name="password" id="password" required>
                         <p style="color: red; margin: 0;">*</p>
                     </div>
                     <div class="col-lg-6 py-2" style="display: flex">
                         <input type="password" class="form-control btnBorder" name="confirmPassword" placeholder="CONFIRMAR CONTRASEÑA" id="confirmPassword" required>
+                        <p style="color: red; margin: 0;">*</p>
+                    </div>
+                    <div class="col-lg-6 py-2" style="display: flex">
+                        <input type="text" class="form-control btnBorder" placeholder="R.F.C" id="rfc" name="rfc" required>
                         <p style="color: red; margin: 0;">*</p>
                     </div>
 
