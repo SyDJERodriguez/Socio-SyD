c1b7a81b2fc2c8010491babf7446fa9d6cef97b6
Frank Reyes <fmartinez@quaxar.com>
2021-06-08 09:46:18 -0500

    Fix responsive design

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -144,7 +144,7 @@
                 <div class="col-lg-6 mx-auto text-center py-1">
                     <div class="bg-primary py-1 divAzul" style="background-color: #00a5e6 !important;">
                         <h4 class="primary-color mt-5 mb-2"><strong>DUEÑO DE NEGOCIO</strong></h4>
-                        <h6 class="text-white mt-5 mb-2"> 
+                        <h6 class="text-white mt-5 mb-2">
                             <strong>Registrate y recibe beneficios para ti y tus colaboradores de negocio</strong>
                         </h6>
                         <img class="my-3" src="{{asset('img/car.png')}}" width="160px" alt="">
@@ -175,7 +175,7 @@
                 <div class="col-lg-6 mx-auto text-center py-1">
                     <div class="bg-primary py-1 divAzul" style="background-color: #00a5e6 !important;">
                         <h4 class="primary-color mt-5 mb-2"><strong>MECÁNICO INDEPENDIENTE</strong></h4>
-                        <h6 class="text-white mt-5 mb-2"> 
+                        <h6 class="text-white mt-5 mb-2">
                             <strong>Registrate y recibe beneficios únicamente para ti</strong>
                         </h6>
                         <img class="my-3" src="{{asset('img/men_2.png')}}" width="160px" alt="">
@@ -191,10 +191,10 @@
                         </div>
 
                         <div>
-                            <img class="my-3 imgTipo" id="tableClientType1" src="{{asset('img/tabla1.png')}}" alt="tablaIndivuales" 
-                            style="max-width: 100%;padding: 0px 30px 56px 30px">
+                            <img class="my-3 imgTipo" id="tableClientType1" src="{{asset('img/tabla1.png')}}" alt="tablaIndivuales"
+                            style="max-width: 100%;padding: 0 30px 0 30px">
                         </div>
-                        
+
 
                         <div class="my-4 mb-5">
                             <button class="btn btn blue-dark text-white btnBorder" data-toggle="modal" data-target="#modal5">
