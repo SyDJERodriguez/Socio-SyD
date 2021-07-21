450051996b4ea1fba842170fad4aeef603704da8
eduardo-af <laguirre@quaxar.com>
2021-07-21 17:35:56 -0500

    addBranchNumber to transaction migration added

---
+++ database/migrations/2021_07_21_172746_add_branch_number_to_transactions.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBranchNumberToTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->bigInteger('branch_number')->nullable()->default(null);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            //
+        });
+    }
+}
