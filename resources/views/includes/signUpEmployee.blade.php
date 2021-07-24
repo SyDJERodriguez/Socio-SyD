a558d5b5632e835c3c58891b493c3f6b70a0db99
eduardo-af <laguirre@quaxar.com>
2021-07-24 11:48:03 -0500

    changed customerId in addEmployee from customer table

--- resources/views/includes/signUpEmployee.blade.php
+++ resources/views/includes/signUpEmployee.blade.php
@@ -20,7 +20,7 @@
                     @method("PUT")
                     @csrf
                     <input type="hidden" name="client_number" value="{{Auth::user()->client_number}}">
-                    <input type="hidden" name="customer_id" value="{{Auth::user()->id}}">
+                    <input type="hidden" name="customer_id" value="{{$data->id}}">
                     <input type="hidden" name="nameClient" value="{{$data->name}}">
                     <input type="hidden" name="lastNameClient" value="{{$data->last_name}}">
                     <input type="hidden" name="branch_number" value="{{ isset($data->branch_number) ? $data->branch_number : null }}">
