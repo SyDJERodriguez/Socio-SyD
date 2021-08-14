812fe851e5c4c119919984de2726f4c74d6af2d2
eduardo-af <laguirre@quaxar.com>
2021-08-13 19:08:32 -0500

    benefits icon separeted in includes, added video perdidaOrganica and logic

---
+++ resources/views/includes/Account/benefitsIcons.blade.php
@@ -0,0 +1,77 @@
+<div class="col-lg-4 text-center py-3" >
+    <h6 style="color: #143153;cursor: pointer;" data-toggle="modal" data-target="#modal8">
+        <img class="py-2"   
+                style="width:150px; height:150px;" 
+                src="{{asset('img/perdida_organica.png')}}" 
+        ><br> <strong class="py-3"> PÉRDIDA ORGÁNICA
+         </strong></h6>
+         <a class="btn btn-outline-light btn-sm"
+              style="background-color: #143153"
+              id="showVideoButton1"
+              href="#"
+              onclick="playVideoBenefit('perdidaOrganica')"
+             >Ver video</a>
+        </div>
+<div class="col-lg-4 py-3 text-center iconInvalid" style="cursor: pointer" data-toggle="modal" data-target="#modal8">
+    <h6 style="color: #143153;"> <img class="py-2"
+        style="width:150px; height:150px;" src="{{asset('img/invalidez_total.png')}}">
+        <br><strong class="py-3"> INVALIDEZ TOTAL <br> Y PERMANENTE <br> 
+            </strong></h6>
+</div>
+<div class="col-lg-4 py-3 text-center" style="cursor: pointer;" data-toggle="modal" data-target="#modal8">
+    <h6 style="color: #143153;"><img class="py-2"
+        style="width:150px; height:150px;" src="{{asset('img/muerte_accidental.png')}}">
+         <br><strong class="py-3"> MUERTE ACCIDENTAL
+        </strong></h6>
+</div>
+<div class="col-lg-4 text-center py-3" style="cursor: pointer;" data-toggle="modal" data-target="#modal8">
+    <h6 style="color: #143153;"><img class="py-2"
+        style="width:150px; height:150px;" src="{{asset('img/reembolso.png')}}">
+        <br> <strong class="py-3">REEMBOLSO DE  <br> GASTOS MÉDICOS  <br>  
+    </strong></h6>
+</div>
+<div class="col-lg-4 py-3 text-center">
+</div>
+<div class="col-lg-4 py-3 text-center" style="cursor: pointer;" data-toggle="modal" data-target="#modal8">
+    <h6 style="color: #143153;"><img class="py-2"
+        style="width:150px; height:150px;" src="{{asset('img/indemnización.png')}}"> <br>
+        <strong class="py-3"> INDEMNIZACIÓN POR ACCIDENTE  
+        </strong></h6>
+</div>
+<p style="color: #143153;font-size: 13px;margin-bottom: 0px;">
+    *Consulta términos y condiciones
+</p>
+
+<!-- Modal VIDEO HOME-->
+<div class="modal fade" id="modalVideoBenefits" tabindex="-1" role="dialog" aria-labelledby="modalVideoBenefits" aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document">
+        <div class="modal-content border-0 rounded-0">
+            <div class="modal-header d-flex flex-row-reverse" style="background-color: #ffffff;">
+                <span class="times" data-dismiss="modal" aria-label="Close">X</span>
+            </div>
+            <div class="modal-body " style="background-color: #143153;padding-top: 0px;padding-bottom: 0px;">
+                <div class="row">
+                    <div class="div">
+                        <video id="videoBenefits" class="videoInsert" controls>
+                            <source id="videoSource"
+                                    src="{{ asset('video/perdidaOrganica.mp4') }}"
+                                    type="video/mp4">
+                          Your browser does not support the video tag.
+                          </video>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<script>
+    function playVideoBenefit(videoName) {
+        let hrefVideo = "{{ asset('video/{name}.mp4') }}";
+        hrefVideo = hrefVideo.replace('{name}',videoName);
+        console.log(hrefVideo);
+        $("#videoSource").src = hrefVideo;
+        $("#modalVideoBenefits").modal('show'); 
+        /* TODO: ya abre video pero no se pausa la reproduccion */
+    }
+</script>
