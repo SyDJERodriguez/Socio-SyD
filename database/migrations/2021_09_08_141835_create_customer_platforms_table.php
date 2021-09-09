f3935093d8bb06b899b5fc2568a2df1c08b5b23a
eduarlalo <laguirre@quaxar.com>
2021-09-09 11:48:26 -0500

    migration rename

--- database/migrations/2021_09_08_141835_create_customer_platforms_table.php
+++ database/migrations/2021_09_08_141835_create_customer_platforms_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomersPlatformTable extends Migration
+class CreateCustomerPlatformsTable extends Migration
 {
     /**
      * Run the migrations.
