e32d4f9627d28a4100af7e243a8db93eb9ee62ef
eduarlalo <laguirre@quaxar.com>
2021-09-27 13:17:25 -0500

    client number added to dependient

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -1122,7 +1122,7 @@ class CustomerController extends Controller
 
         //Insert data in customers table
         $update_customer = DB::table('customer_platforms')->insert([
-            'client_number'    => null,
+            'client_number'    => $request['client_number'],
             'name'             => $request['name'],
             'last_name'        => $request['last_name'],
             'second_last_name' => $request['second_last_name'],
@@ -2351,7 +2351,7 @@ class CustomerController extends Controller
         try {
             Mail::send('emails.invitacionAsociadoNew',['data'=>$data], function($m) use ($email){
                 $m->from('sociosyd@syd.com.mx',"Socio SYD");
-                $m->to($email)->subject("Da clic en el botón y activa tu cuenta de Socio SYD");
+                $m->to($email)->subject("Te han invitado a ser parte del programa Socio SYD, haz clic en el botón para aceptar la invitación");
             });
         } catch (\Throwable $th) {
             //throw $th;
