18a5589c82140fab407dbce041525486f251f6e2
eduarlalo <laguirre@quaxar.com>
2021-10-07 11:35:05 -0500

    new emails replaced

--- resources/views/emails/sinSeguroInvitacionSeguroDuenio30Mes.blade.php
+++ resources/views/emails/sinSeguroInvitacionSeguroDuenio30Mes.blade.php
@@ -8,28 +8,28 @@
 <table id="Tabla_01" width="600" height="" border="0" cellpadding="0" cellspacing="0" align="center" style="display: block">
 	<tr>
 		<td colspan="3">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-19_01.jpg" width="600" height="545" alt="" border="0" style="display: block"></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Socio_SyD_18_01.jpg" width="600" height="545" alt="" border="0" style="display: block"></td>
 	</tr>
 	<tr>
 		<td colspan="3">
 			<a href="https://www.sociosyd.com/" target="_blank">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-19_02.jpg" width="600" height="115" alt="" border="0" style="display: block"></a></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Socio_SyD_18_02.jpg" width="600" height="85" alt="" border="0" style="display: block"></a></td>
 	</tr>
 	<tr>
 		<td colspan="3">
 			<a href="https://www.sociosyd.com/" target="_blank">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-19_03.jpg" width="600" height="119" alt="" border="0" style="display: block"></a></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Socio_SyD_18_03.jpg" width="600" height="150" alt="" border="0" style="display: block"></a></td>
 	</tr>
 	<tr>
 		<td>
 			<a href="https://sociosyd.com/" target="_blank">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-footer_01.jpg" width="225" height="49" alt="" border="0" style="display: block"></a></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Footer_01.jpg" width="225" height="51" alt="" border="0" style="display: block"></a></td>
 		<td>
 			<a href="https://www.facebook.com/SyDMexico/" target="_blank">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-footer_02.jpg" width="125" height="49" alt="" border="0" style="display: block"></a></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Footer_02.jpg" width="140" height="51" alt="" border="0" style="display: block"></a></td>
 		<td>
 			<a href="https://www.instagram.com/syd.mexico/" target="_blank">
-			<img src="https://resources.quaxar.net/SyD/Socio_SyD/email-footer_03.jpg" width="250" height="49" alt="" border="0" style="display: block"></a></td>
+			<img src="https://resources.quaxar.net/SyD/Socio_SyD/Footer_03.jpg" width="235" height="51" alt="" border="0" style="display: block"></a></td>
 	</tr>
 </table>
 </body>
