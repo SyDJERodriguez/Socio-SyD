cb4bab14fcad757aa6160e110d8cb273d46b82c7
Jesus Eduardo <jvazquez@quaxar.com>
2021-12-06 13:09:35 -0600

    order in form cnt

--- resources/views/includes/formularioMecanicoCNT.blade.php
+++ resources/views/includes/formularioMecanicoCNT.blade.php
@@ -107,14 +107,16 @@
                     <div class="col-lg-6 py-2" style="display: flex">
                         <input autocomplete="new-password" type="password" class="form-control" placeholder="CONTRASEÑA" name="password" id="password" pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[-¡!$%^&*()_+|~=`{}\[\]:@;'<>¿?,.\/]).{8,20}$" title="La contraseña debe tener más de 8 caracteres, una mayúscula, un número y un caracter especial " required>
                         <p style="color: red; margin: 0;">*</p>
+                        <label class="labelgre py-2" style="top: 35px; font-size:10.2px;">* Debe contener mayúsculas, minúsculas, números y caracteres especiales</label>
+                        <br><br>
                     </div>
                 </div>
                 <div class="row ">
-                    <div class="col-lg-6 py-2" style="display: flex">
+                    <div class="col-lg-6 order-1 order-lg-0 py-2" style="display: flex">
                         <input type="text" class="form-control" placeholder="R.F.C" id="rfcCNT" name="rfc">
                         <p style="color: red; margin: 0;visibility: hidden">*</p>
                     </div>
-                    <div class="col-lg-6 py-2" style="display: flex">
+                    <div class="col-lg-6 order-0 order-lg-1 py-2" style="display: flex">
                         <input type="password" class="form-control" placeholder="CONFIRMAR CONTRASEÑA" name="confirmPassword" id="confirmPassword" required>
                         <p style="color: red; margin: 0;">*</p>
                     </div>
