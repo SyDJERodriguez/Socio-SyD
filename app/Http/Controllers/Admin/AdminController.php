3f103c2722b51391dc322c65a5a210c6c0f37bb2
Frank Reyes <fmartinez@quaxar.com>
2022-02-03 12:44:56 -0600

    validation for customer session in cat

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -102,6 +102,11 @@ class AdminController extends Controller
             ->where('client_number', '=', $client_number)
             ->get();
 
+        if ( $customerData->isEmpty() ){
+            $error = 'El usuario solicitado no se encuentra registrado en el programa Socio SyD';
+            return view('Admin.customer', compact('error'));
+        }
+
         $customerDatabranch = DB::table('client_numbers')
             ->where('client_number', '=', $client_number)
             ->get();
@@ -141,7 +146,7 @@ class AdminController extends Controller
         $current_month = $now->month;
         $current_year = $now->year;
         $previus_month =$now->month - 1;
-        
+
         if ($previus_month == 0) {
             $current_year = $now->year-1;
             $previus_month =$now->month + 11;
