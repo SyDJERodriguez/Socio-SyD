c20e3176c56522042f6c4f5ee92a7f701620d7d2
Frank Reyes <fmartinez@quaxar.com>
2022-02-09 13:32:52 -0600

    Add function for generate PDF with curent month

--- routes/web.php
+++ routes/web.php
@@ -40,6 +40,7 @@ Route::put('/addEmployeebranch', 'CustomerController@addEmployeebranch')->name('
 // Routes without login
 /**  Download PDF by SMS **/
 Route::get('/sms_pdf/{client_number}/{branch_number}', 'BeneficiaryController@generatePDFSMS')->name('sms.generate.pdf');
+Route::get('/pdf/{email}/', 'BeneficiaryController@generatePDFSMS')->name('email.generate.pdf');
 Route::get('/register/beneficiaries/{email}', function ($email){
     $data = DB::table('customer_platforms')->where('email', $email)->first();
     $data_session = DB::table('customers_sessions')->where('email', $email)->first();
